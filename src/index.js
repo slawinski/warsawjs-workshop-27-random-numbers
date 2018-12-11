167171705ebd8df1389e6d6eca3fb49286f6f9a5
piotr-s <piotr.slawinski1@gmail.com>
2018-12-11 20:18:43 +0100

    Implement code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 module.exports = {
-    run() {
-        console.log('Hello world');
+    arrOfRand(n = 6) {
+        // const arr = Array(n);
+
+        const arr = [];
+        for (let i = 0; i < n; i++) {
+            arr.push(this.randNum());
+        }
+        return arr;
+    },
+
+    randNum(lBound = 1, uBound = 81) {
+        let min = Number(lBound);
+        let max = Number(uBound);
+        return Math.ceil(Math.random() * (max - min) + min); // eslint-disable-line
     }
 };
